00b40e645fbd753ddd5616dda55f3a1706af4a2e
purpleclay <purpleclaygh@gmail.com>
2023-07-02 07:19:28 +0100

    upload latest dist

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,6 @@ async function run(): Promise<void> {
 
     await core.group('Running NSV', async () => {
       const nextSemVer = await nsv(download.path, nextOnly ? 'next' : 'tag')
-      core.info(nextSemVer)
       core.setOutput('nsv', nextSemVer)
     })
   } catch (error) {
